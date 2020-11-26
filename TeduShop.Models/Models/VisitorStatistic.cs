c2c5df3f5594e66c44291323c0ea0e8cc81e5d19
cuong1998pro <cuong1998pro@gmail.com>
2020-11-26 15:31:10 +0700

    xay lop data

--- TeduShop.Models/Models/VisitorStatistic.cs
+++ TeduShop.Models/Models/VisitorStatistic.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TeduShop.Models.Models
 {
     [Table("VisitorStatistics")]
-    public class VisitorStatistics
+    public class VisitorStatistic
     {
         [Key]
         public Guid ID { get; set; }
